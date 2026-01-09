a89ed85a5c11042df982287d3ca1d8510cf9bdb3
dharshan08092005 <dharshanpersonal01@gmail.com>
2026-01-09 12:37:03 +0530

    boilerplate

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">  
+        </div>
+    </>
+  )
+}
+
+export default App
